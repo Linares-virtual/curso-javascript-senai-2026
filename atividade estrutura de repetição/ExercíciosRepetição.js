d75fbf89b3f8818a9543a5cebca43b457b28a132
Alexandre Linares <118585608+Linares-virtual@users.noreply.github.com>
2026-03-31 10:49:34 -0300

    feat: atividade estrutura de repetição

---
+++ atividade estrutura de repetição/ExercíciosRepetição.js
@@ -0,0 +1,86 @@
+
+    // EXERCÍCIO DE REPETIÇÃO:
+
+    // // 1. Contagem de 1 a 10:
+    // // Use um for para exibir os números de 1 a 10 no console.
+
+
+    // for (let i = 1; i <= 10; i++) {
+    //     console.log("Numeros: " , i)
+    // }
+
+
+    // //------------------------------------------------------------------------------------------
+    
+    // // 2. Tabuada de um número:
+    // // - Peça um número ao usuário.
+    // // - Exiba a tabuada desse número de 1 a 10 usando um for.
+
+
+    //    let numero = prompt("Digite um número para a tabuada:");
+
+    //    for (let i = 1; i <= 10; i++) {
+    //    console.log(numero + " x " + i + " = " + (numero * i));4
+    // }
+
+    // //-------------------------------------------------------------------------------------------
+
+
+    // 3. Soma dos primeiros N números naturais:
+    // - Peça um número N ao usuário.
+    // - Use um while ou for para somar os números de 1 até N.
+    // - Exemplo:
+    // - Se um usuário inserir o número 4, o código deverá fazer a soma do 
+    // 1, 2, 3 e 4 totalizando 10.           
+    // - Se um usuário inserir o número 6, o código deverá fazer a soma do 1, 2, 3, 4 , 5 e 6 totalizando 21.
+
+    // let n = prompt("Somar até qual número?");
+    // let soma = 0;
+
+    // for (let i = 1; i <= n; i++) {
+    //     soma += i;
+    // }
+    // console.log("Resultado da soma: " + soma);
+
+
+
+    // //--------------------------------------------------------------------------------------------
+
+
+    // Intermediário:
+
+    // 1. Exibir os números pares de 1 a 50:
+    // - Use um for ou while para exibir apenas os números pares de 1 a 50.
+
+    // console.log("--- Números Pares (1 a 50) ---");
+    // for (let i = 2; i <= 50; i += 2) {
+    //     console.log(i);
+    // }
+
+
+    // ----------------------------------------------------------------------------------------------
+
+
+    // 4. Validação de senha
+    //     - Crie uma senha fixa (ex: "1234")
+    //     - Peça ao usuário até ele acertar
+    //     - Use while
+
+
+    // console.log("--- Sistema de Segurança ---");
+
+    // const senhaCorreta = "1234";
+    // let senhaDigitada = "";
+
+    // while (senhaDigitada !== senhaCorreta) {     
+
+    //     senhaDigitada = prompt("Digite a senha de acesso:");
+
+    //     if (senhaDigitada !== senhaCorreta) {
+    //         alert("Senha incorreta! Tente novamente.");
+    //         console.log("Tentativa de acesso negada.");
+    //     }
+    // }
+    // alert("Acesso concedido! Bem-vindo.");
+    // console.log("Acesso autorizado com sucesso.");
+
